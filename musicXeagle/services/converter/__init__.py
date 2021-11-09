4194988f9c1c48264be3c442dbd7cdb62843a893
Mustache Beard <87588953+Kingache@users.noreply.github.com>
2021-11-10 00:06:30 +0700

    Typo

--- musicXeagle/services/converter/__init__.py
+++ musicXeagle/services/converter/__init__.py
@@ -3,6 +3,6 @@ from os import listdir, mkdir
 if "raw_files" not in listdir():
     mkdir("raw_files")
 
-from eagleXmusic.services.converter.converter import convert
+from musicXeagle.services.converter.converter import convert
 
 __all__ = ["convert"]
