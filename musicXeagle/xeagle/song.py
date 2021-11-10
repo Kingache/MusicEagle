c8201731327f2ea37117f5677e069094dae22fc6
Mustache Beard <87588953+Kingache@users.noreply.github.com>
2021-11-11 01:39:34 +0700

    Update song.py

--- musicXeagle/xeagle/song.py
+++ musicXeagle/xeagle/song.py
@@ -18,6 +18,7 @@ from pyrogram.types import Message
 from youtube_search import YoutubeSearch
 from youtubesearchpython import SearchVideos
 
+from musicXeagle.config import BOT_USERNAME as bn
 from musicXeagle.config import DURATION_LIMIT
 from musicXeagle.xeagle.play import arq
 
@@ -59,7 +60,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 Uploaded by Eagle Music**"
+        rep = "**🎵 Uploaded by @{bn}**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
